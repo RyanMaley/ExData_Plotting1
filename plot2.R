8ae9609f113b599e9fc99f3f74810e72650661c1
Ryan Maley <ryan@maley.org>
2015-10-09 12:33:36 -0500

    Project 1 Submission
    Contains code files and PNG outputs for course project 1.

---
+++ plot2.R
@@ -0,0 +1,34 @@
+## Program: plot1.R
+##
+## Coursera exdata-032: Exploratory Data Analysis
+## Author: Ryan Maley
+##
+##
+plot2 <- function (){
+        ##
+        ## Produces a Global Active Power by Day Plot
+        ##        
+        ZipArchive <- tempfile()
+        ZipArchive <- "exdata_data_household_power_consumption.zip"
+        
+        ## Download the files - Uncomment when live
+        ##        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",ZipArchive)
+
+        ## Read the data and cut it based on the date
+        TheData <- read.csv(unz(ZipArchive,"household_power_consumption.txt"),
+                sep=";",stringsAsFactors = FALSE)
+        TheData <- TheData[(TheData$Date=="1/2/2007" | TheData$Date=="2/2/2007"),]
+        
+        ## Convert the date from chr to date class, the time to POSIXlt
+        TheData$Date <- as.Date(TheData$Date,"%d/%m/%Y")
+        TheData$Time <- strptime(paste(TheData$Date, TheData$Time), format = "%Y-%m-%d %H:%M:%S")
+        
+        ## Create the PNG based file
+        png(filename="plot2.png")
+        
+        ## Generate the line chart for Plot 2
+        plot(TheData$Time,as.numeric(TheData$Global_active_power),type="l",ylab="Global Active Power (kilowatts)")
+        
+        ## Close the graphics device
+        dev.off()
+}        
